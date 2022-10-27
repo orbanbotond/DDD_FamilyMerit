06332703219aa791ee786e2e33da86d866198f33
Orban Botond <orbanbotond@gmail.com>
2022-10-27 12:02:01 +0300

    Add the listeners to the Order Read model

--- rails-application/app/read-models/fullfillment/order.rb
+++ rails-application/app/read-models/fullfillment/order.rb
@@ -1,46 +1,58 @@
 module Fullfillment
   class Order < ApplicationRecord
-    # class Configuration
-    #   def call(cqrs)
-    #     @cqrs = cqrs
+    class Configuration
+      def call(cqrs)
+        @cqrs = cqrs
 
-    #     subscribe_and_link_to_stream(
-    #       ->(event) { record_transaction(event) },
-    #       [
-    #         # Payments::Cards::Events::Create,
-    #         # Payments::Cards::Events::Deliver,
-    #         # Payments::Cards::Events::Authorized,
-    #         # Payments::Cards::Events::Captured,
-    #         # Payments::Cards::Events::Released,
-    #       ]
-    #     )
-    #   end
+        subscribe_and_link_to_stream(
+          ->(event) { record_transaction(event) },
+          [
+            Fullfillments::Orders::Events::Created,
+            Fullfillments::Orders::Events::Delivered,
+            Fullfillments::Orders::Events::Aborted,
+            Fullfillments::Orders::Events::DeliveryFailed
+          ]
+        )
+      end
 
-    #   private
+      private
 
-    #   def subscribe_and_link_to_stream(handler, events)
-    #     link_and_handle = ->(event) do
-    #       link_to_stream(event)
-    #       handler.call(event)
-    #     end
-    #     subscribe(link_and_handle, events)
-    #   end
+      def subscribe_and_link_to_stream(handler, events)
+        link_and_handle = ->(event) do
+          link_to_stream(event)
+          handler.call(event)
+        end
+        subscribe(link_and_handle, events)
+      end
 
-    #   def link_to_stream(event)
-    #     @cqrs.link_event_to_stream(event, "ReportOrders$all")
-    #   end
+      def link_to_stream(event)
+        @cqrs.link_event_to_stream(event, "ReportOrders$all")
+      end
 
-    #   def subscribe(handler, events)
-    #     @cqrs.subscribe(handler, events)
-    #   end
+      def subscribe(handler, events)
+        @cqrs.subscribe(handler, events)
+      end
 
-    #   def record_transaction(event)
-    #     Transaction.create transaction_id: event.data[:id],
-    #                        amount: amount,
-    #                        order_id: 'n/a',
-    #                        transaction_type: transaction_type[event.class]
-    #   end
-    # end
+      def record_transaction(event)
+        case event
+        when Fullfillments::Orders::Events::Created
+          Order.create fullfillment_id: event.data[:id]
+        when Fullfillments::Orders::Events::Delivered
+          order = order_found_from_event(event)
+          order.update delivered: true
+        when Fullfillments::Orders::Events::Aborted
+          order = order_found_from_event(event)
+          order.update not_delivered: true
+        when Fullfillments::Orders::Events::DeliveryFailed
+          order = order_found_from_event(event)
+          order.update not_delivered: true
+        end
+      end
+
+      def order_found_from_event(event)
+        Order.find_by fullfillment_id: event.data[:id]
+      end
+    end
 
     self.table_name = "fullfillment_reports"
     # t.string "fullfillment_id"
--- rails-application/lib/configuration.rb
+++ rails-application/lib/configuration.rb
@@ -3,30 +3,11 @@ require_relative "../../merits/configuration"
 
 class Configuration
   def call(cqrs)
-    # enable_res_infra_event_linking(cqrs)
-
     enable_domain(cqrs)
     enable_read_models(cqrs)
-
-    # Ecommerce::Configuration.new(
-    #   number_generator: Rails.configuration.number_generator,
-    #   payment_gateway: Rails.configuration.payment_gateway,
-    #   available_vat_rates: [
-    #     Infra::Types::VatRate.new(code: "10", rate: 10),
-    #     Infra::Types::VatRate.new(code: "20", rate: 20)
-    # ]
-    # ).call(cqrs)
   end
 
   private
-
-  # def enable_res_infra_event_linking(cqrs)
-  #   [
-  #     RailsEventStore::LinkByEventType.new,
-  #     RailsEventStore::LinkByCorrelationId.new,
-  #     RailsEventStore::LinkByCausationId.new
-  #   ].each { |h| cqrs.subscribe_to_all_events(h) }
-  # end
 
   def enable_domain(cqrs)
     Merits::Configuration.new.call(cqrs)
@@ -37,5 +18,6 @@ class Configuration
   def enable_read_models(cqrs)
     TimeHarvest::Report::Configuration.new.call(cqrs)
     Fullfillment::Transaction::Configuration.new.call(cqrs)
+    Fullfillment::Order::Configuration.new.call(cqrs)
   end
 end
